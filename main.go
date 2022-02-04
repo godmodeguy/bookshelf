4e308de1fc5c978ffc889b852e96fbc48d7f2a39
wintermute <godmodguy@gmail.com>
2022-02-04 17:42:35 +0300

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"learn/easyrest/controllers"
+	"learn/easyrest/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+import "github.com/swaggo/gin-swagger" // gin-swagger middleware
+import "github.com/swaggo/files"       // swagger embed files
+import _ "learn/easyrest/docs"
+
+// @title        Book API
+// @version      0.0.1
+// @description  Simple REST API for managing books
+
+// @host      localhost:8080
+// @BasePath  /
+
+func main() {
+	r := gin.Default()
+	models.ConnectDB()
+
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PUT("/books/:id", controllers.UpdateBook)
+	r.POST("/books", controllers.CreateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run()
+}
