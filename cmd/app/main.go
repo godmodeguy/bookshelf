f93ba600cdbe6dfae34b348551396a2ce99feb3f
wintermute <godmodguy@gmail.com>
2022-02-09 22:01:01 +0300

    upload

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,7 +13,7 @@ import _ "learn/easyrest/docs"
 
 // @title        Book API
 // @version      0.0.1
-// @description  Simple REST API for managing books
+// @description  Simple REST API for managing bookse
 
 // @host      localhost:8080
 // @BasePath  /
@@ -22,12 +22,26 @@ func main() {
 	r := gin.Default()
 	models.ConnectDB()
 
+	// auth
+	r.GET("/auth/register", controllers.Register)
+	r.GET("/auth/login", controllers.Login)
+
+	// ok, lets (write) something
+	// there we go 
+	/*
+	that is hellno
+
+	*/
+
+	// books api
+	// something else? swagger tested
 	r.GET("/books", controllers.FindBooks)
 	r.GET("/books/:id", controllers.FindBook)
 	r.PUT("/books/:id", controllers.UpdateBook)
 	r.POST("/books", controllers.CreateBook)
 	r.DELETE("/books/:id", controllers.DeleteBook)
 
+	// swagger
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run()
